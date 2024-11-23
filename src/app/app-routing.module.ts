34b753044af09ac72f584cfe0ce18764e4c9882d
Jean <oka.jeanjaures@gmail.com>
2024-11-23 21:59:48 +0100

    config: define default root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SigninComponent} from "./features/signin/signin.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {SignupComponent} from "./features/signup/signup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Default root : launch page
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  //Others root
+  {path : 'signin',component:SigninComponent},
+  {path : 'signup',component:SignupComponent},
+  //Root for page not found
+  {path : '**',redirectTo:'/signin'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  FormsModule,ReactiveFormsModule,BrowserModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
