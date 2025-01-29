b1048beefbe9df06b330780e2ec88070aa21ad5d
Jean <oka.jeanjaures@gmail.com>
2025-01-29 14:34:43 +0100

    feat (layout) : to list all booking per month

---
+++ src/app/_features/booking/booking.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-booking',
+  templateUrl: './booking.component.html',
+  styleUrl: './booking.component.css'
+})
+export class BookingComponent implements OnInit{
+
+
+
+
+
+  constructor() {
+  }
+  onSubmit(){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  getToPreviousMonth(){
+
+  }
+
+  getToNextMonth(){
+
+  }
+
+}
