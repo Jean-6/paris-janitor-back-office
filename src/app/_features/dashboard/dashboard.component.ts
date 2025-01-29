4be82ab988c81e42a7f00f5bcda6528fcc54cf3f
Jean <oka.jeanjaures@gmail.com>
2025-01-29 15:32:30 +0100

    feat (layout) : add chart grid into dashboard

--- src/app/_features/dashboard/dashboard.component.ts
+++ src/app/_features/dashboard/dashboard.component.ts
@@ -1,21 +1,27 @@
 import {Component, OnInit} from '@angular/core';
+import {ChartModule} from "primeng/chart";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {AsideMenuComponent} from "../shared/aside-menu/aside-menu.component";
 
 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  standalone: true,
+  imports: [ChartModule, RouterLink, RouterOutlet, AsideMenuComponent]
 })
-
-
 
 export class DashboardComponent implements OnInit{
 
 
-  ngOnInit(): void {
+  constructor() {
+  }
 
+  ngOnInit() {
 
   }
+
 
 }
