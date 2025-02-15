e38b30d53e95bf6183bfb871f8a2b7b9d7c6a590
Jean <oka.jeanjaures@gmail.com>
2025-02-15 17:08:31 +0100

    config (interceptor Http) : Manage authentication and refresh tokens automatically

---
+++ src/app/_interceptors/authInterceptor.ts
@@ -0,0 +1,27 @@
+
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest} from "@angular/common/http";
+import {catchError, Observable, switchMap, throwError} from "rxjs";
+import {AuthService} from "../_services/auth.service";
+import {inject} from "@angular/core";
+
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+
+  const authService = inject(AuthService);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if(error.status === 401) {
+        return authService.refreshToken().pipe(
+          switchMap(() => {
+            return next(req);
+          }),
+          catchError((refreshError)=>{
+            authService.logout();
+            return throwError(() => refreshError);
+          })
+        );
+      }
+      return throwError(() => error);
+    })
+  );
+}
